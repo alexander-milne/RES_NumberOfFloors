9072b7c9f31bb3b636cc3d526f988d059ea6688e
alexander-milne <43757854+alexander-milne@users.noreply.github.com>
2020-06-22 17:06:16 +0100

    Init Files

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace RessApp
+{
+    class Program
+    {
+        public static NoOfAssigmentsStore nOAS;
+
+        public static double NumberOfArrangements(double numberOfStories)
+        {
+            string arranment = "";
+            for (double i = 0; i < numberOfStories; i++)
+            {
+                arranment += "s";
+            }
+
+            String[] array = new String[] { arranment };
+            nOAS.noOfAssignmentsStore[0] = 0;
+            nOAS.noOfAssignmentsStore[1] = 1;
+            double val = 1 + noOfOptionsAddingOneLarge(arranment);
+            return val;
+        }
+
+        public static double noOfOptionsAddingOneLarge(string arranment)
+        {
+            
+            if (nOAS.noOfAssignmentsStore[arranment.Length-1] != -1)
+            {
+                return nOAS.noOfAssignmentsStore[arranment.Length-1];
+            }
+
+
+            string newArranment = arranment.Substring(2, arranment.Length - 2);
+            string newArranment_only_one = arranment.Substring(1, arranment.Length - 1);
+            nOAS.noOfAssignmentsStore[arranment.Length-1] = 1 + noOfOptionsAddingOneLarge(newArranment) + noOfOptionsAddingOneLarge(newArranment_only_one);
+            return nOAS.noOfAssignmentsStore[arranment.Length-1];
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int numberOfFloors = 1399;
+            nOAS = new NoOfAssigmentsStore(numberOfFloors);
+            Console.WriteLine(NumberOfArrangements(numberOfFloors));
+        }
+    }
+}
---
+++ noOfAssigmentsStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RessApp
+{
+    class NoOfAssigmentsStore
+    {
+        public double[] noOfAssignmentsStore;
+
+        public NoOfAssigmentsStore(int size)
+        {
+            if (size < 2)
+            {
+                size = 2;
+            }
+            noOfAssignmentsStore = new double[size];
+            
+            for (var i = 0; i < size; i++)
+            {
+                noOfAssignmentsStore[i] = -1;
+            }
+        }
+    }
+}
